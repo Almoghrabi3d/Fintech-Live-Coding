d5bcb5df6cada2097e6ee69ed823c060f01857c9
Almoghrabi3d <94754098+Almoghrabi3d@users.noreply.github.com>
2025-10-06 21:54:58 +0300

    Web1  Done

---
+++ Web2/Marwan.java
@@ -0,0 +1,10 @@
+
+
+public class Marwan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}import java.util.Scanner;
